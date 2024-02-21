40950f8be95fdf2f3147526ef7684558c682eb8f
jacobdhlee <jacobdhlee@jacobs-mbp-2.dhcp.wustl.edu>
2024-02-21 14:16:33 -0600

    Finished!

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,36 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		double x = 0.5;
+		double y = 0.5;
+		double halfwidth = 0.18;
+		double halflength = 0.1;
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(x, y, halfwidth, halflength);
+		
+		double x1 = 0.5;
+		double y1 = 0.5;
+		double radius = 0.06;
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledCircle(x1, y1, radius);
+		
+		StdDraw.setPenRadius(0.005);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.rectangle(x, y, halfwidth, halflength);
+		
+		double x2 = 0.5;
+		double y2 = 0.5;
+		double halfwid = 0.07;
+		double halfleng = 0.005;
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(x2, y2, halfwid, halfleng);
+		
+		double x3 = 0.5;
+		double y3 = 0.5;
+		double halfwide = 0.005;
+		double halflong = 0.07;
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledRectangle(x3, y3, halfwide, halflong);
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,47 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		String shape = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double a = in.nextDouble();
+		double b = in.nextDouble();
+		double c = in.nextDouble();
+		double d = in.nextDouble();
 		
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		if (shape.equals("ellipse")) {
+			
+			if (isFilled == true) {
+				StdDraw.filledEllipse(a, b, c, d);
+			}
+			if (isFilled == false) {
+				StdDraw.ellipse(a, b, c, d);	
+			}
+		}
+
+		if (shape.equals("rectangle")) {
+			if (isFilled == true) {
+				StdDraw.filledRectangle(a, b, c, d);
+			}
+			if (isFilled == false) {
+				StdDraw.rectangle(a, b, c, d);	
+			}
+		}
+		
+		if (shape.equals("triangle")) {
+			double e = in.nextDouble();
+			double g = in.nextDouble();
+			double[] x = {a, c, e};
+			double[] y = {b, d, g};
+			if (isFilled == true) {
+				StdDraw.filledPolygon(x, y);
+			}
+			if (isFilled == false) {
+				StdDraw.polygon(x, y);	
+			}
+		}
 	}
 }
